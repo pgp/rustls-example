22cd5e9311bd541dff41a87a8a2a37267538539d
pgp <pgp@gmx.it>
2020-02-27 16:29:27 +0100

    build with old libs, works on android, sends headers

---
+++ cross_compile_android_abis_release.sh
@@ -0,0 +1,18 @@
+cargo clean
+export PATH=$PATH:/home/pgp/Android/Sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin
+
+export CC=armv7a-linux-androideabi19-clang
+export CXX=armv7a-linux-androideabi19-clang++
+cargo build --target armv7-linux-androideabi --release
+
+export CC=aarch64-linux-android21-clang
+export CXX=aarch64-linux-android21-clang++
+cargo build --target aarch64-linux-android --release
+
+export CC=i686-linux-android19-clang
+export CXX=i686-linux-android19-clang++
+cargo build --target i686-linux-android --release
+
+export CC=x86_64-linux-android21-clang
+export CXX=x86_64-linux-android21-clang
+cargo build --target x86_64-linux-android --release
