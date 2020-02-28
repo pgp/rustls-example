e4b498f1bd21eaa661ecd964f328da0d3b0c565f
pgp <pgp@gmx.it>
2020-02-28 11:02:11 +0100

    remove comments

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,3 @@
-//fn main() -> Result<(), Box<std::error::Error>> {
-//    let resp: HashMap<String, String> = reqwest::get("https://httpbin.org/ip")?
-//        .json()?;
-//    println!("{:#?}", resp);
-//    println!("HelloWorld 123571113171923");
-//    Ok(())
-//}
-
-//extern crate futures;
-//extern crate hyper;
-//extern crate tokio_core;
-//
-//use futures::{Future};
-//use hyper::{Client, Uri};
-//use tokio_core::reactor::Core;
-//
-//fn main() {
-//    // Core is the Tokio event loop used for making a non-blocking request
-//    let mut core = Core::new().unwrap();
-//
-//    let client = Client::new(&core.handle());
-//
-//    let url : Uri = "http://httpbin.org/response-headers?foo=bar".parse().unwrap();
-//    assert_eq!(url.query(), Some("foo=bar"));
-//
-//    let request = client.get(url)
-//        .map(|res| {
-//            assert_eq!(res.status(), hyper::Ok);
-//        });
-//
-//    // request is a Future, futures are lazy, so must explicitly run
-//    core.run(request).unwrap();
-//
-//    println!("All OK");
-//}
-
 //! Simple HTTPS GET client based on hyper-rustls
 //!
 //! First parameter is the mandatory URL to GET.
